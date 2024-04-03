782bb7fa65248785fb0543ee95dd542f0d6ec6db
Adrian Doonan <adriandoonan@gmail.com>
2024-04-03 15:54:22 +0200

    initial expense routes

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ require("./config")(app);
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
+const expenseRoutes = require("./routes/expenses.routes");
+app.use("/expenses", expenseRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
