8a75c4102d035cb6063002b4912d059d285d5f42
shaza kashlan <eng.shazakashlan@gmail.com>
2024-04-09 08:36:00 +0200

    add cloudinary middleware and routes

--- models/User.model.js
+++ models/User.model.js
@@ -1,44 +1,44 @@
 const { Schema, model } = require("mongoose");
-const Address = require("./Address.model");
+const { userAddressSchema } = require("./Address.model");
 
-// TODO: Please make sure you edit the User model to whatever makes sense in this case
+// Define User Schema
 const userSchema = new Schema(
-	{
-		emailAddress: {
-			type: String,
-			required: [true, "Email is required."],
-			unique: true,
-			lowercase: true,
-			trim: true,
-		},
-		password: {
-			type: String,
-			required: [true, "Password is required."],
-		},
-		userName: {
-			type: String,
-			required: [true, "UserName is required."],
-		},
-		fullName: {
-			type: String,
-			default: "",
-		},
-		address: {
-			type: Schema.Types.ObjectId,
-			ref: "UserAddress",
-			default: new Address(),
-		},
-		image: {
-			type: String,
-			default: "",
-		},
-	},
-	{
-		// this second object adds extra properties: `createdAt` and `updatedAt`
-		timestamps: true,
-	},
+  {
+    emailAddress: {
+      type: String,
+      required: [true, "Email is required."],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+    userName: {
+      type: String,
+      required: [true, "UserName is required."],
+    },
+    fullName: {
+      type: String,
+      default: "",
+    },
+    address: userAddressSchema,
+    imageUrl: {
+      type: String,
+      default: "",
+    },
+    mobileNumber: {
+      type: String,
+    },
+  },
+  {
+    // Enable timestamps for createdAt and updatedAt fields
+    timestamps: true,
+  }
 );
 
+// Create User model
 const User = model("User", userSchema);
 
 module.exports = User;
