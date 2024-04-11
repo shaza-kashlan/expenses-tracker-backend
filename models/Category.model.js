d05e8cc0e91087a863035b80c57158fb2d0617f8
Adrian Doonan <adriandoonan@gmail.com>
2024-04-11 11:49:39 +0200

    include icon in expense response, icon as string for category

--- models/Category.model.js
+++ models/Category.model.js
@@ -5,7 +5,7 @@ const categorySchema = new Schema(
     name: { type: String, required: true, unique: true },
     description: String,
     parent_category: { type: Schema.Types.ObjectId, ref: "Category" },
-    icon: { type: Schema.Types.ObjectId, ref: "Icon" },
+    icon: { type: String, default: "🤷" },
     public: { type: Boolean, default: true },
     created_by_user_id: { type: Schema.Types.ObjectId, ref: "User" },
     patterns: {type: [String]}
